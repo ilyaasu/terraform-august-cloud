e965824bbbc781a3bf0cf2d93edd6f34f98ba55a
elias solebo <88907629+ilyaasu@users.noreply.github.com>
2021-11-18 02:03:23 +0100

    added 3

--- ec2.tf
+++ ec2.tf
@@ -27,6 +27,14 @@ resource "aws_instance" "web2" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld2"
+  }
+}
+resource "aws_instance" "web3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld3"
   }
 }
